a440566ac5a8d3a30e09bcde3014b82c7c5a9510
rorikwong <108846354+rorikwong@users.noreply.github.com>
2025-02-07 15:18:50 -0600

    changing up some icons and adding context and part controllers

---
+++ Controllers/PartsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TurboKanban.Data;
+namespace TurboKanban.Controllers;
+[Route("parts")]
+[ApiController]
+public class PartsController : Controller
+{
+    private readonly PartsContext _db;
+
+    public PartsController(PartsContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Parts>>> GetParts()
+    {
+        return (await _db.Parts.ToListAsync()).OrderByDescending(s => s.IsAccquired).ToList();
+    }
+}
---
+++ Data/PartsContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace TurboKanban.Data;
+
+public class PartsContext : DbContext
+{
+    public PartsContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Parts> Parts { get; set; }
+}
